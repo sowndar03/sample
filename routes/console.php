98d6f2f536a9db962f1e068c52ea10d8969bcda1
sowndar <sowndar.b@ardhas.com>
2026-05-27 12:29:13 +0530

    Revert "added"
    This reverts commit 263ec10b2c7e0749d855c2e6a5d162bbeae0c425.

--- routes/console.php
+++ routes/console.php
@@ -6,6 +6,3 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-
-##fist commit
